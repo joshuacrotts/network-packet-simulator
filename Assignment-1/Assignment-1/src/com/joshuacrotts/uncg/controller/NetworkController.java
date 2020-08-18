23ee0b378f94570d74115d04f500144a8bfe3a7c
Joshua Crotts <joshuacrotts@yahoo.com>
2020-08-18 12:20:14 -0400

    Removed crappy Swing code and added Standards.

--- Assignment-1/Assignment-1/src/com/joshuacrotts/uncg/controller/NetworkController.java
+++ Assignment-1/Assignment-1/src/com/joshuacrotts/uncg/controller/NetworkController.java
@@ -1,84 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.joshuacrotts.uncg.controller;
 
-import com.joshuacrotts.uncg.view.NetworkView;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import com.revivedstandards.main.StandardDraw;
+import com.revivedstandards.main.StandardGame;
+import java.awt.Color;
 
 /**
  *
  * @author joshuacrotts
  */
-public class NetworkController implements Runnable {
-
-  /*
-   * Model and view objects according to my standard.
-   */
-  private final NetworkView view;
-
-  /*
-   * Thread for the animation loop/actions.
-   */
-  private Thread thread;
-
-  /*
-   * Thread information.
-   */
-  private final int FRAMES_PER_SECOND = 60;
-  private boolean isRunning = false;
-
-  /*
-   * Positioning and title of the view.
-   */
-  private final String TITLE = "Network Simulator Assignment 1";
-  private final int WIDTH = 800;
-  private final int HEIGHT = 600;
+public class NetworkController extends StandardGame {
 
   public NetworkController() {
-    this.view = new NetworkView(WIDTH, HEIGHT, TITLE);
-
-    this.startController();
+    super(800, 600, "Network Simulator - Assignment 1");
+    
+    this.startGame();
   }
 
-  /**
-   *
-   * @Override default runnable procedure in Runnable interface.
-   */
   @Override
-  public void run() {
-    while (true) {
-      try {
-        Thread.sleep(FRAMES_PER_SECOND);
-      } catch (InterruptedException ex) {
-        Logger.getLogger(NetworkController.class.getName()).log(Level.SEVERE, null, ex);
-      }
-    }
+  public void tick() {
   }
 
-  /**
-   *
-   */
-  private synchronized void startController() {
-    if (this.isRunning) {
-      return;
-    }
-
-    this.thread = new Thread(this);
-    this.thread.start();
-    this.isRunning = true;
-  }
-
-  /**
-   *
-   */
-  private synchronized void stopController() {
-    if ( ! this.isRunning) {
-      return;
-    }
-
-    try {
-      this.thread.join();
-    } catch (InterruptedException ex) {
-      Logger.getLogger(NetworkController.class.getName()).log(Level.SEVERE, null, ex);
-    }
+  @Override
+  public void render() {
+    StandardDraw.drawEllipse(20,20, 300, 300, Color.yellow, true);
   }
 }
